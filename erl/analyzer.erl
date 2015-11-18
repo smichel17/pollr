decb42ef8171729da7a4fa72ef414ad3ffd3e27b
Seth Kahn <kahns729@gmail.com>
2015-11-18 16:59:58 -0500

    erl-to-python interaction working

--- erl/analyzer.erl
+++ erl/analyzer.erl
@@ -1,2 +1,8 @@
 -module(analyzer).
--export([analyze/1]).
+-export([analyze/2]).
+
+analyze(Tweet, _Hashtag) ->
+	{ok, P} = python:start([{python_path, "./python"},
+                         	{python, "python3"}]),
+	python:call(P, sentiment, analyze, [Tweet]).
+	
--- python/sentiment.py
+++ python/sentiment.py
@@ -17,7 +17,7 @@ def readList(filename):
 	return happy_log_probs, sad_log_probs
 
 # Naive Bayes Analysis
-def analyse_sentiment(sentence, happy, sad):
+def analyze_sentiment(sentence, happy, sad):
 	useless_words = []
 	tokens = sentence.rstrip().split(" ")
 	happy_log_prob = numpy.sum([happy[token] for token in tokens if token in happy and token not in useless_words])
@@ -25,6 +25,14 @@ def analyse_sentiment(sentence, happy, sad):
 	happy_prob = numpy.reciprocal(numpy.exp(sad_log_prob - happy_log_prob) + 1)
 	sad_prob = 1 - happy_prob
 	return happy_prob, sad_prob
+
+def analyze(sentence):
+	sentence = ''.join(chr(i) for i in sentence)
+	happy_log_probs, sad_log_probs = readList("./wordlists/top20000.txt")
+	happy_prob, sad_prob = analyze_sentiment(sentence, happy_log_probs, sad_log_probs)
+	# return (happy_prob, sad_prob)
+	# return "P(happy) = " + str(happy_prob) + "\nP(sad)   = " + str(sad_prob)
+	return float(happy_prob), float(sad_prob)
 
 def main(argv):
 	if len(argv) < 2:
@@ -34,7 +42,7 @@ def main(argv):
 	sample = input("")
 	# First argument is filename of word list
 	happy_log_probs, sad_log_probs = readList(argv[1])
-	happy_prob, sad_prob = analyse_sentiment(sample, happy_log_probs, sad_log_probs)
+	happy_prob, sad_prob = analyze_sentiment(sample, happy_log_probs, sad_log_probs)
 	print("P(happy) = " + str(happy_prob))
 	print("P(sad)   = " + str(sad_prob))
 	return 0
