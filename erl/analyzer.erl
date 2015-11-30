0719799ce6b4e48b3afd2d0b00ec5c1a30a09e46
Seth Kahn <kahns729@gmail.com>
2015-11-29 20:58:51 -0500

    scraper in erlang

--- erl/analyzer.erl
+++ erl/analyzer.erl
@@ -5,5 +5,7 @@ analyze(Tweet, Hashtag) ->
 	{ok, P} = python:start([{python_path, "./python"},
                          	{python, "python3"}]),
 	% Send back the tuple of happy probability and sad probability
-	Hashtag ! python:call(P, sentiment, analyze, [Tweet]).
+	Hashtag ! python:call(P, sentiment, analyze, [Tweet])
+	python:stop(P)
+	.
 	
--- erl/scraper.erl
+++ erl/scraper.erl
@@ -1,2 +1,13 @@
 -module(scraper).
--export([scrape/1]).
+-export([scrape/2]). %scrape/1s
+
+
+
+scrape(Query, From) ->
+	{ok, P} = python:start([{python_path, "./python"},
+                         	{python, "python3"}]),
+	% Send back the tuple of happy probability and sad probability
+	From ! python:call(P, scraper, search, [Query]),
+	python:stop(P)
+	.
+
