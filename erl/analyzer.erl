680d3faecdb6adc07be49de72b9b8946a237b5ae
gabe-terrell <Gabe.C.Terrell+GitHub@gmail.com>
2015-11-30 11:11:44 -0500

    Fixed Formatting of Erlang Files
    Also a missing comma

--- erl/analyzer.erl
+++ erl/analyzer.erl
@@ -5,7 +5,5 @@ analyze(Tweet, Hashtag) ->
 	{ok, P} = python:start([{python_path, "./python"},
                          	{python, "python3"}]),
 	% Send back the tuple of happy probability and sad probability
-	Hashtag ! python:call(P, sentiment, analyze, [Tweet])
-	python:stop(P)
-	.
-	
+	Hashtag ! python:call(P, sentiment, analyze, [Tweet]),
+	python:stop(P).
--- erl/scraper.erl
+++ erl/scraper.erl
@@ -1,13 +1,9 @@
 -module(scraper).
 -export([scrape/2]). %scrape/1s
 
-
-
 scrape(Query, From) ->
 	{ok, P} = python:start([{python_path, "./python"},
                          	{python, "python3"}]),
 	% Send back the tuple of happy probability and sad probability
 	From ! python:call(P, scraper, search, [Query]),
-	python:stop(P)
-	.
-
+	python:stop(P).
