7b36359ea17b91bc788856e9d3745af4ee9df16f
gabe-terrell <Gabe.C.Terrell+GitHub@gmail.com>
2015-12-02 16:47:26 -0500

    Cleaned Up Scraper Module

--- erl/scraper.erl
+++ erl/scraper.erl
@@ -4,11 +4,8 @@
 scrape(Query) ->
 	{ok, P} = python:start([{python_path, "./python"},
                          	{python, "python3"}]),
+	% Generate a list of tweets matching the query
     Tweetlist = python:call(P, scraper, search, [Query]),
-    % THIS COMMENT IS DEPRECATED:
-	% Send back the tuple of happy probability and sad probability
+    % Send to master the average sentiment of the resulting tweets
     whereis(master) ! {score, Query, analyzer:analyze_tweets(Tweetlist)}, 
 	python:stop(P).
-
-
-analyze(Tweetlist) -> 0.5. %stub
